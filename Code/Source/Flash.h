9b24be76b0e949857b516c4a1e6853f09fa80268
cs <cs1597616514@gmail.com>
2025-09-10 15:06:17 +0800

    init 来自c037

---
+++ Code/Source/Flash.h
@@ -0,0 +1,42 @@
+#ifndef FLASH_H
+#define FLASH_H
+
+//系统存储器：出厂由ST在这个区域内部预置了一段BootLoader，也就是ISP程序，这是一块ROM
+//没有什么中低容量产品，只会用C8和R8，均为64K，	 每页1KB。系统存储器3KB。
+#define FLASH_ADDR_IAP_START 			0x08000000		//IAP=7K，这个地方出了一次问题，修改后大于6K，侧面反映编译出来的Zi-data也是flash的东西
+#define FLASH_ADDR_APP_START 			0x08001C00		//APP=64-7-1-1=55K
+
+#define FLASH_ADDR_SH367309_VALUE 		0x0800F000		//休眠带电关键参数(电流偏置值)，1K
+#define FLASH_ADDR_SH367309_FLAG 		0x0800F400		//休眠是否带电标志位，1K
+
+#define FLASH_ADDR_UPDATE_FLAG 			0x0800F800		//升级标志位，1K
+#define FLASH_ADDR_SLEEP_FLAG           0x0800FC00		//休眠关键指令，1K
+
+
+#define FLASH_309_RTC_RTC_VALUE			((UINT16)0x1222)	//RTC休眠，RTC唤醒，只有RTC休眠，才有可能RTC唤醒。
+#define FLASH_309_RTC_NORMAL_VALUE		((UINT16)0x2333)	//RTC休眠，别的形式唤醒(通讯，按钮等等)
+#define FLASH_309_NORMAL_NORMAL_VALUE	((UINT16)0xFFFF)	//普通休眠，别的形式唤醒(通讯，按钮等等)
+
+
+#define FLASH_TO_IAP_VALUE				((UINT16)0x00AB)
+#define FLASH_TO_APP_VALUE				((UINT16)0xFFFF)
+
+#define FLASH_NORMAL_SLEEP_VALUE    	((UINT16)0x1234)
+#define FLASH_DEEP_SLEEP_VALUE    		((UINT16)0x1235)
+#define FLASH_HICCUP_SLEEP_VALUE    	((UINT16)0x1236)
+#define FLASH_SLEEP_RESET_VALUE    		((UINT16)0xFFFF)
+
+
+#define MCU_RESET()	NVIC_SystemReset()
+
+
+FLASH_Status FlashWriteOneWord(uint32_t Address, uint32_t Data);
+UINT32 FlashReadOneWord(UINT32 faddr);
+
+FLASH_Status FlashWriteOneHalfWord(uint32_t StartAddr,uint16_t Buffer);
+UINT16 FlashReadOneHalfWord(UINT32 faddr);
+void App_FlashUpdateDet(void);
+void Init_IAPAPP(void);
+
+#endif	/* FLASH_H */
+
---
+++ Code/Source/NewFunc/LedBar.h
@@ -0,0 +1,72 @@
+#ifndef LEDBAR_H
+#define LEDBAR_H
+
+typedef enum _LEDBAR_COMMAND {
+	LED_BAR_STARTUP = 0,
+    LED_BAR_NORMAL,
+    LED_BAR_CHG,
+    LED_BAR_DSG,
+	LED_BAR_FAULT,
+}LEDBAR_COMMAND;
+
+// #define MCUI_SOC_KEY 		(PORT_IN_GPIOB->bit14)
+
+// #define MCUO_SOC_20 		(PORT_OUT_GPIOB->bit7)
+// #define MCUO_SOC_40 		(PORT_OUT_GPIOB->bit8)
+// #define MCUO_SOC_60 		(PORT_OUT_GPIOB->bit13)
+// #define MCUO_SOC_80 		(PORT_OUT_GPIOB->bit12)
+// #define MCUO_SOC_100 		(PORT_OUT_GPIOB->bit5)
+
+// #define MCUO_SOC_RUN 		(PORT_OUT_GPIOA->bit5)
+// #define MCUO_SOC_ALARM 		(PORT_OUT_GPIOA->bit6)
+#define PORT_SOC_20             GPIOA
+#define PIN_SOC_20	            GPIO_Pin_4
+
+#define PORT_SOC_40             GPIOA
+#define PIN_SOC_40	            GPIO_Pin_5
+
+#define PORT_SOC_60             GPIOA
+#define PIN_SOC_60	            GPIO_Pin_6
+
+#define PORT_SOC_80             GPIOA
+#define PIN_SOC_80	            GPIO_Pin_7
+
+#define PORT_SOC_100            GPIOB
+#define PIN_SOC_100	            GPIO_Pin_7
+
+#define PORT_SOC_RUN             GPIOC
+#define PIN_SOC_RUN	            GPIO_Pin_14
+
+#define PORT_SOC_ALM             GPIOC
+#define PIN_SOC_ALM	            GPIO_Pin_15
+
+#define PORT_SOC_BLE             GPIOF
+#define PIN_SOC_BLE	            GPIO_Pin_7
+
+#define PORT_SOC_KEY             GPIOB
+#define PIN_SOC_KEY              GPIO_Pin_5
+
+
+#define MCUO_SOC_20_OFF          (PORT_SOC_20->BSRR |= PIN_SOC_20 <<16)
+#define MCUO_SOC_20_ON          (PORT_SOC_20->BSRR |= PIN_SOC_20)
+#define MCUO_SOC_20_TOGGLE      (PORT_SOC_20->ODR ^= PIN_SOC_20)
+
+#define MCUO_SOC_20 		(PORT_OUT_GPIOA->bit4)
+#define MCUO_SOC_40 		(PORT_OUT_GPIOA->bit5)
+#define MCUO_SOC_60 		(PORT_OUT_GPIOA->bit6)
+#define MCUO_SOC_80 		(PORT_OUT_GPIOA->bit7)
+#define MCUO_SOC_100 		(PORT_OUT_GPIOB->bit7)
+
+#define MCUO_SOC_RUN 		(PORT_OUT_GPIOC->bit14)
+#define MCUO_SOC_ALARM 		(PORT_OUT_GPIOC->bit15)
+//ble run½»»»       ºóÐøÌæ»»
+#define MCUI_SOC_KEY 		(PORT_IN_GPIOB->bit5)
+//FIXME IN OR OUT
+#define MCUO_SOC_BLE 		(PORT_OUT_GPIOF->bit7)
+
+// #define MCUO_SOC_BLE 		(PORT_OUT_GPIOC->bit14)
+// #define MCUO_SOC_RUN 		(PORT_OUT_GPIOF->bit7)
+
+void APP_LedBar(void);
+
+#endif	/* LEDBAR_H */
