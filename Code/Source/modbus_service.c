9f4bf8e8211fa6520a0f65f0c93e9994cefadb5e
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-08-25 22:31:23 +0800

    fix(comm): suppress Modbus broadcast responses

--- Code/Source/modbus_service.c
+++ Code/Source/modbus_service.c
@@ -1,4 +1,5 @@
 #include "modbus_service.h"
+#include "modbus_proto.h"
 #include <string.h>
 
 void Modbus_ServiceInit(struct RS485MSG *msg)
@@ -9,12 +10,15 @@ void Modbus_ServiceInit(struct RS485MSG *msg)
 uint16_t Modbus_ServiceHandleFrame(struct RS485MSG *msg, const uint8_t *frame, uint16_t frame_len, uint8_t *tx_buf, uint16_t tx_capacity)
 {
     uint16_t i;
+    uint8_t is_broadcast;
 
     Sci_DataInit(msg);
     if ((frame_len == 0) || (frame_len > RS485_MAX_BUFFER_SIZE))
     {
         return 0;
     }
+
+    is_broadcast = (uint8_t)(frame[0] == MODBUS_BROADCAST_ADDR);
 
     for (i = 0; i < frame_len; ++i)
     {
@@ -25,6 +29,10 @@ uint16_t Modbus_ServiceHandleFrame(struct RS485MSG *msg, const uint8_t *frame, u
     switch (msg->u16Buffer[1])
     {
     case RS485_CMD_READ_REGS:
+        if (is_broadcast != 0U)
+        {
+            return 0;
+        }
         msg->enRs485CmdType = RS485_CMD_READ_REGS;
         break;
     case RS485_CMD_WRITE_REG:
@@ -38,6 +46,11 @@ uint16_t Modbus_ServiceHandleFrame(struct RS485MSG *msg, const uint8_t *frame, u
     }
 
     Sci_ModbusService_Execute(msg);
+    if (is_broadcast != 0U)
+    {
+        return 0;
+    }
+
     if ((msg->AckLenth == 0) || (msg->AckLenth > RS485_MAX_BUFFER_SIZE) || (msg->AckLenth > tx_capacity))
     {
         return 0;
